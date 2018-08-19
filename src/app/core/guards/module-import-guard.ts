07fff396f56a3249317ae65703366ac7806b311a
Ted Weatherly <ted.weatherly@gmail.com>
2018-08-19 17:24:58 -0500

    undo temp change

--- src/app/core/guards/module-import-guard.ts
+++ src/app/core/guards/module-import-guard.ts
@@ -1,4 +1,4 @@
-export function throwIfAlreadyLoaded2(parentModule: any, moduleName: string): void {
+export function throwIfAlreadyLoaded(parentModule: any, moduleName: string): void {
   if (parentModule) {
     const errorMsg: string = `${moduleName} has already been loaded. Import Core modules in the AppModule only.`;
     throw new Error(errorMsg);
